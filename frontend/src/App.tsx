e50299605479f78f4ff94c4be80c9694bceb6c72
Vitor Paixão <vitornvpaixao@gmail.com>
2026-04-27 19:27:23 +0100

    feat(ws): add first comunication with ws

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,28 @@
 import './style/App.css';
 import Rectangle from './components/Rectangle';
+import { type movedObject } from './types';
+import { useState } from 'react';
 
 function App() {
+  // Will set position for rectangle component update postion on ws msg
+  const [position, setPosition] = useState({isActive: false, x: 0, y: 0});
+  const ws = new WebSocket('ws://localhost:8080');
   
+  ws.addEventListener("open", () => {
+    console.log("Connection established");
+  });
+
+  ws.addEventListener("message", (event) => {
+    console.log("Text message:", event.data);
+  });
+
+  const handlePositionChange = (info: movedObject) => {
+    ws.send(JSON.stringify(info));
+  }
+
   return (
     <div>
-      <Rectangle />
+      <Rectangle id="rect-1" className="red-rectangle" onPositionChange={handlePositionChange} newPosition={position}/>
     </div>
   );
 }
