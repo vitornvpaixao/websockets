0727bc5c1d12614c00d717674821a2aa21ce9464
Vitor Paixão <vitornvpaixao@gmail.com>
2026-04-27 19:30:19 +0100

    feat(rectangle): add ref for dragging, remote position updates, minor updates

--- frontend/src/components/Rectangle.tsx
+++ frontend/src/components/Rectangle.tsx
@@ -1,11 +1,20 @@
+import { useRef } from "react";
 import useDraggable from "../hooks/useDraggable";
 
-export default function Rectangle() {
-  const { beginDrag, dragging, endDragging } = useDraggable();
-  
+export default function Rectangle(props: any) {
+  const elementRef = useRef(null);
+  const { beginDrag, dragging, endDragging, applyPosition } = useDraggable(elementRef, props.id, props.onPositionChange);
+
+  // To improve - apply position received from ws
+  if (props.newPosition.isActive) {
+    applyPosition(props.newPosition.x, props.newPosition.y);
+  }
+
   return (
       <div
-        className="red-rectangle"
+        ref={elementRef}
+        id={props.id}
+        className={props.className}
         onPointerDown={beginDrag}
         onPointerMove={dragging}
         onPointerUp={endDragging}
