3d2387f50bc6ca47350ed18c9289032a74805cdc
Vitor Paixão <vitornvpaixao@gmail.com>
2026-04-28 19:36:49 +0100

    refactor(draggable): simplify drag state and pointer release handling

--- frontend/src/hooks/useDraggable.tsx
+++ frontend/src/hooks/useDraggable.tsx
@@ -1,61 +1,51 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { type movedObject } from "../types";
 
 interface dragState {
     isDragging: boolean;
     offSetX: number;
     offSetY: number;
-    releasePointer: Function | null
 }
 
-function useDraggable(elemREF: any, elemID: number | string, onPositionChange: Function){
-    // Should i change this to ref instead state ??
-    const [dragState, setDragState] = useState<dragState>({isDragging: false, offSetX: 0, offSetY: 0, releasePointer: null});
-    
+function useDraggable(elemId: string, onPositionChange: Function){
+    const [ dragState, setDragState ] = useState<dragState>({isDragging: false, offSetX: 0, offSetY: 0});
+    const pointerReleaseRef = useRef<() => void | null>(null);
+
     const beginDrag = (e: React.PointerEvent<HTMLDivElement>): void => {
         const target = e.currentTarget as HTMLElement;
-        const pointerID = e.pointerId;
+        const pointerId = e.pointerId;
 
         setDragState({
             isDragging: true,
             offSetX: e.clientX - target.getBoundingClientRect().left,
             offSetY: e.clientY - target.getBoundingClientRect().top,
-            releasePointer: () => target.releasePointerCapture(pointerID)
         })
 
-        target.setPointerCapture(pointerID);
+        pointerReleaseRef.current = () => target.releasePointerCapture(pointerId)
+        target.setPointerCapture(pointerId);
     }
 
     const dragging = (e: React.PointerEvent<HTMLDivElement>): string | void => {
         if (!dragState.isDragging) return;
         const newX = e.clientX - dragState.offSetX;
         const newY = e.clientY - dragState.offSetY;
-        // Sendmsg to ws with movement information
+        
+        // Send message to WebSocket with movement information
         const movedObject: movedObject = {
-            id: elemID,
+            id: elemId,
             x: newX,
             y: newY 
         }
 
-        applyPosition(newX, newY);
         onPositionChange(movedObject);
     }
 
     const endDragging = (): void => {
         setDragState(prev => ({...prev, isDragging: false}));
-        
-        if (!dragState.releasePointer) return;
-        dragState.releasePointer();
+        pointerReleaseRef.current?.();
     }
 
-    const applyPosition = (x: number, y: number): void => {
-        if (!elemREF.current) return;
-        const elem = elemREF.current as HTMLElement;
-
-        elem.style.transform = `translate(${x}px, ${y}px)`
-    }
-
-    return { beginDrag, dragging, endDragging, applyPosition }
+    return { beginDrag, dragging, endDragging }
 }
 
 export default useDraggable;
