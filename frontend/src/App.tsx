5b764e7c9e1f27090a32ace06a5f2b84ea1f7fec
Vitor Paixão <vitornvpaixao@gmail.com>
2026-04-28 19:33:14 +0100

    refactor(ws): improve WebSocket connection logic and add HTML elements

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,106 @@
 import './style/App.css';
 import Rectangle from './components/Rectangle';
 import { type movedObject } from './types';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
+
+interface elementPosition {
+  id: string | number;
+  isActive: boolean;
+  x: number;
+  y: number;
+}
 
 function App() {
-  // Will set position for rectangle component update postion on ws msg
-  const [position, setPosition] = useState({isActive: false, x: 0, y: 0});
-  const ws = new WebSocket('ws://localhost:8080');
-  
-  ws.addEventListener("open", () => {
-    console.log("Connection established");
-  });
+  const [ position, setPosition ] = useState<elementPosition>({id: '', isActive: false, x: 0, y: 0});
+  const [ isConnected, setIsConnected ] = useState<boolean>(false);
+  const throttleRef = useRef<number | null>(null);
+  const webSocketRef = useRef<WebSocket>(null);
 
-  ws.addEventListener("message", (event) => {
-    console.log("Text message:", event.data);
-  });
+  useEffect(() => {
+    let cleanUp: (() => void) | null = null;
+
+    const connectWithRetry = (attempt = 0) => {
+      const maxRetries = 5;
+      const delay = Math.min(1000 * Math.pow(2, attempt), 10000);
+      const socket = new WebSocket('ws://localhost:8080');
+      webSocketRef.current = socket;
+
+      const handleOpen = () => {
+        console.log("Connection established. Attempt: ", attempt);
+        setIsConnected(true);
+      };
+
+      const handleClose = () => {
+        if (attempt < maxRetries) {
+          console.log('WebSocket Closed')
+          console.log(`Retry Connect WebSocket in ${delay}ms`)
+          setTimeout(() => {
+            connectWithRetry(attempt + 1);
+          }, delay);
+        } else {
+          console.log('WebSocket Closed - No more attempts')
+        }
+      }
+
+      const handleMessage = (event: MessageEvent) => {
+        const parsedData = JSON.parse(event.data);
+
+        setPosition({id: parsedData.id, isActive: true, x: parsedData.x, y: parsedData.y})
+      };
+
+      const handleError = (err: Event) => {
+        console.error('WebSocket error: ', err);
+        setIsConnected(false);
+      };
+      
+      socket.addEventListener('open', handleOpen);
+      socket.addEventListener('message', handleMessage);
+      socket.addEventListener('error', handleError);
+      socket.addEventListener('close', handleClose)
+
+      cleanUp = () => {
+        socket.removeEventListener('message', handleMessage);
+        socket.removeEventListener('error', handleError);
+        socket.removeEventListener('open', handleOpen);
+        socket.removeEventListener('close', handleClose);
+
+        socket.close();
+      }
+    }
+
+    connectWithRetry();
+    
+    return () => {
+      cleanUp?.();
+    };
+  }, []);
 
   const handlePositionChange = (info: movedObject) => {
-    ws.send(JSON.stringify(info));
+    setPosition({id: info.id, isActive: true, x: info.x, y: info.y})
+    
+    if (!throttleRef.current) {
+        throttleRef.current = setTimeout(() => {
+          if (isConnected && webSocketRef.current?.readyState === WebSocket.OPEN) {
+            webSocketRef.current?.send(JSON.stringify(info));
+          }
+          throttleRef.current = null;
+        }, 16)
+    }
   }
 
   return (
     <div>
-      <Rectangle id="rect-1" className="red-rectangle" onPositionChange={handlePositionChange} newPosition={position}/>
+      <Rectangle id="rect-1" className="green-rectangle" onPositionChange={handlePositionChange} newPosition={position}/>
+      <Rectangle id="rect-2" className="red-rectangle" onPositionChange={handlePositionChange} newPosition={position}/>
+      <Rectangle id="rect-3" className="blue-rectangle" onPositionChange={handlePositionChange} newPosition={position}/>
+
+      <button
+        onClick={() => {
+          webSocketRef.current?.close();
+        }}
+      >
+        Simulate socket close
+      </button>
     </div>
   );
 }
