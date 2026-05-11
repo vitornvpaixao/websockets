21c6cb6901799db4a8c7ad1bed1a2adf1934a381
Vitor Paixão <vitornvpaixao@gmail.com>
2026-05-11 13:42:01 +0100

    feat(web-socket-chat): boilerplate + initial conection logic in server

---
+++ real-time-chat-app/backend/src/server.ts
@@ -0,0 +1,99 @@
+import WebSocket, { WebSocketServer } from 'ws';
+import { config } from '../config';
+
+const { PORT, HOST } = config;
+const wss = new WebSocketServer({ port: PORT });
+
+const activeConnections: Connection[] = [];
+const users: User[] = [];
+
+type Connection = {
+    ws: WebSocket,
+    user: User
+}
+
+type User = {
+    id: string | number;
+    name?: string;
+    connectAt: Date;
+    conversations: Conversation[];
+}
+
+type Conversation = {
+    userId: string | number;
+    messages: Message[];
+}
+
+type Message = {
+    date: Date;
+    text: string;
+}
+
+// improve the logs for each needed section. We must have a way to control users logged, when and who!
+wss.on('connection', (ws: WebSocket) => {
+    
+    ws.on('error', console.error);
+
+    // On message we have to control wich client is sending message and for who is to send the message;
+    // we will need as well an obj with good structure to know who is sending messages an to who!
+    // we will need a king of DB (we can use initialy and array) and then save and get from Local Storage
+    // we will need different methods to control adding users and create the conversation
+    // incremet conversation messagens, etc
+    ws.on('message', (data: any, isBinary: any) => {
+        const processedData = JSON.parse(data.toString());
+        console.log(`[${new Date().toISOString()}] received: ${processedData}`);
+        console.log(processedData.type)
+        
+        // Here we must update DB with new user
+        // This works as a connection validation
+        if (processedData.type === 'identify') {
+            let user = users.find((user) => user.id === processedData.userId)
+
+            if (!user) {
+                user = {
+                    id: processedData.userId,
+                    connectAt: new Date(),
+                    conversations: []
+                }
+
+                // Historic in future
+                users.push(user);
+            }
+
+            const client: Connection = {
+                ws,
+                user: user,
+            }
+            
+            activeConnections.push(client)
+
+            console.log('Active Connections: ', activeConnections);
+
+            return;
+        }
+
+        if (processedData.type === 'message') { 
+            // wss.clients.forEach(cli => {
+            //     if (cli !== ws && cli.readyState === WebSocket.OPEN) {
+            //         cli.send(data, {binary: isBinary}, (err) => {
+            //             if (err) console.error('WS send error: ', err);
+            //             }
+            //         );
+            //     }
+            // })
+        }
+    });
+
+     ws.on('close', () => {
+        console.log('Socket Closed')
+        const userIdx = activeConnections.findIndex((connection: Connection) => connection.ws === ws);
+
+        if (userIdx !== -1) {
+            activeConnections.splice(userIdx, 1);
+        }
+
+        console.log(activeConnections)
+    });
+});
+
+console.log(`Server running at: ws://${HOST}:${PORT}`);
