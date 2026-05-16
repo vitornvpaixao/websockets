7878f4e2cf94d15899dd7e5ca56c39f608b3381d
Vitor Paixão <vitornvpaixao@gmail.com>
2026-05-16 16:30:39 +0100

    refactor(server): add defensive try catch block on message

--- real-time-chat-app/backend/src/server.ts
+++ real-time-chat-app/backend/src/server.ts
@@ -16,30 +16,45 @@ wss.on('connection', (ws: WebSocket) => {
 
     // On message we have to control which client is sending message and for who is to send the message;
     ws.on('message', (data: Buffer, isBinary: boolean | undefined) => {
-        const parsedData: IClientMessage = JSON.parse(data.toString());
+        try {
+            const parsedData: IClientMessage = JSON.parse(data.toString());
 
-        // This works as a connection validation
-        if (parsedData.type === 'connect_user') {
-            console.log('chegou connect')
-            // update DB with new user (active users)
-            connectUser(ws, parsedData, isBinary)
+            // This works as a connection validation
+            if (parsedData.type === 'connect_user') {
+                console.log('chegou connect')
+                // update DB with new user (active users)
+                connectUser(ws, parsedData, isBinary)
+    
+                return;
+            }
+    
+            if (parsedData.type === 'disconnect_user') { 
+                // remove user from DB (active users)
+                disconnectUser(ws, parsedData, isBinary);
 
-            return;
-        }
-
-        if (parsedData.type === 'disconnect_user') { 
-            // remove user from DB (active users)
-            disconnectUser(ws, parsedData, isBinary);
-        }
-
-        // Create system to receive, save, and send messages to users
-        if (parsedData.type === 'new_message') {
-            // This section will process every received message;
-
-            // Use case 1 - User A send msg to User B
-            // Find User A and save message sended to user B
-            // Find User B and save message received from user A
-            // Send message to user B
+                return;
+            }
+    
+            // Create system to receive, save, and send messages to users
+            if (parsedData.type === 'new_message') {
+                // This section will process every received message;
+    
+                // Use case 1 - User A send msg to User B
+                // Find User A and save message sended to user B
+                // Find User B and save message received from user A
+                // Send message to user B
+            }
+        } catch (e) {
+            // Since we have FE message control, this could be redundant
+            // In other hand this is kind of defensive programming 
+            if (ws.readyState === WebSocket.OPEN) {
+                ws.send(JSON.stringify({ 
+                    type: 'error',
+                     message: 'Invalid message format' 
+                }));
+            } else {
+                console.log(`[${new Date().toISOString()}][Error] Malformed message received on closed socket`);
+            }
         }
     });
 
